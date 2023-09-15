4cc27a92b56a21f7b3e988b1410c76a78a9e45d7
chanel50 <106768673+Chanel50@users.noreply.github.com>
2023-09-16 01:00:47 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Add your code here
+
+function submitData(name, email) {
+    const formData = {
+      name: name,
+      email: email
+    };
+  
+    const configurationObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(formData)
+    };
+  
+    return fetch("http://localhost:3000/users", configurationObject)
+      .then(response => response.json())
+      .then(data => {
+        appendIdToDOM(data.id);
+      })
+      .catch(error => {
+        appendErrorToDOM(error.message);
+      });
+  }
+  
+  function appendIdToDOM(id) {
+    const newElement = document.createElement("p");
+    newElement.innerHTML = `ID: ${id}`;
+    document.body.appendChild(newElement);
+  }
+  
+  function appendErrorToDOM(error) {
+    const errorElement = document.createElement("p");
+    errorElement.innerHTML = `Error: ${error}`;
+    document.body.appendChild(errorElement);
+  }
+  
+  // Example usage:
+  submitData("Chanel hamel", "chanelhamel@example.com");
+  
